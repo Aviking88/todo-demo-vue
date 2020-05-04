17c53e7859b6b2e80cf306e1f73daa2115ea6aa5
Avinash Maurya <avinash.gic@gmail.com>
2020-05-04 19:02:52 +0530

    #Login component added

---
+++ src/components/Login.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="login-container">
+        <h1 v-if="isLogin">Login 👤</h1>
+      <h1 v-else>Register 👤</h1>
+      <div class="form-control">
+        <input name="name" placeholder="Username" v-model="name" />
+      </div>
+      <div class="form-control">
+        <input name="password" placeholder="Password" v-model="password" />
+      </div>
+      <div v-if="!isLogin" class="form-control">
+        <input name="confirm-password" placeholder="Confirm Password" v-model="cnfPassword" />
+      </div>
+
+      <button type="submit">{{ isLogin ? 'Log in 🔒' : 'Register 👤' }} </button>
+      <div class="navigation-link"
+       @click="isLogin = !isLogin"
+       >{{ isLogin ? 'Create new account.' : 'Use existing account.' }}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'svinsh',
+      password: '',
+      cnfPassword: '',
+      isLogin: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.login-container {
+  background: #fff;
+  padding: 50px;
+  width: 250px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 20px 14px 8px rgba(0, 0, 0, 0.58);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.form-control input {
+  border: none;
+  border-bottom: 1px solid #ccc;
+  margin-bottom: 20px;
+  transition: all 300ms ease-in-out;
+  width: 100%;
+}
+.form-control input:focus {
+  outline: 0;
+  border-bottom: 1px solid #666;
+}
+button {
+  margin-top: 20px;
+  background: black;
+  color: white;
+  padding: 10px 0;
+  width: 200px;
+  border-radius: 25px;
+  text-transform: uppercase;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 300ms ease-in-out;
+}
+button:hover {
+  transform: translateY(-2.5px);
+  box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.58);
+}
+h1 {
+  margin: 10px 20px 30px 20px;
+  font-size: 40px;
+}
+.navigation-link{
+    display: block;
+    margin-top: 30px;
+    cursor: pointer;
+    color: #42b983;
+}
+</style>
